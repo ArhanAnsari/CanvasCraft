7219d61c136338136369a53156bdea55c8e5d1cc
Arhan Ansari <arhanansari2009@gmail.com>
2025-08-30 13:58:16 +0530

    fix: Correct base URL logic in getBaseUrl function and update package dependencies

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,10 +1,10 @@
 const getBaseUrl = () =>
   process.env.NODE_ENV === "development"
     ? `https://http://localhost:3000/`
-    : `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+    : `https://canvascraft-ten.vercel.app/`;
 
   process.env.NODE_ENV === "production"
-  ? `https://https://canvascraft-ten.vercel.app/`
-  : `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  ? `https://canvascraft.appwrite.network/`
+  : `https://canvascraft-ten.vercel.app/`;
 
 export default getBaseUrl;
