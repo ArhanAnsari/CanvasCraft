6c1b33d175f5aa40f14264df31fc2cff88a213d7
Arhan Ansari <arhanansari2009@gmail.com>
2025-09-09 23:41:34 +0530

    Update Logo Size in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,8 +11,8 @@ export default function Footer() {
             <Image
               src="/canvascraft-logo.png"
               alt="CanvasCraft Logo"
-              width={36}
-              height={36}
+              width={48}
+              height={48}
               className="rounded"
             />
             <h3 className="text-2xl font-extrabold bg-gradient-to-r from-indigo-400 via-sky-400 to-cyan-300 bg-clip-text text-transparent">
